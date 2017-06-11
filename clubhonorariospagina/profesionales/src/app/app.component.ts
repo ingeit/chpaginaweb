d312407740930bd37ede96adc861a5ddf8fc8dbf
MasterK <masterk@macpro.local>
2017-06-11 19:53:05 -0300

    theming menu

--- clubhonorariospagina/profesionales/src/app/app.component.ts
+++ clubhonorariospagina/profesionales/src/app/app.component.ts
@@ -14,7 +14,7 @@ import { NuevoprofesionalPage } from '../pages/nuevoprofesional/nuevoprofesional
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = NuevoprofesionalPage; 
+  rootPage: any = HomePage; 
 
   pages: Array<{title: string, component: any}>;
 
