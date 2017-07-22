2d3a02bbe437759c616843a35046165dac984dba
MasterK <masterk@macpro.local>
2017-07-21 21:38:00 -0300

    trabajando en el canvas

--- clubhonorariospagina/clientes/src/pages/home/home.ts
+++ clubhonorariospagina/clientes/src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormularioWebPage } from '../formulario-web/formulario-web';
 
@@ -8,9 +8,38 @@ import { FormularioWebPage } from '../formulario-web/formulario-web';
 })
 export class HomePage {
 
+  @ViewChild('myCanvas') canvas: any;
+  @ViewChild('imagen') imagen: any;
+  canvasElement: any;
+  imagenHTML: any;
+  ctx:any;
+  urlImagenCanvas:any;
+
   constructor(public navCtrl: NavController) {
 
+
   }
+
+    ngAfterViewInit(){
+        this.canvasElement = this.canvas.nativeElement;
+        this.imagenHTML = this.imagen.nativeElement;
+        this.ctx = this.canvasElement.getContext('2d');
+    }
+
+    escribrir(){
+        this.canvasElement.width = this.imagenHTML.width;
+        this.canvasElement.crossOrigin = "Anonymous";
+        this.canvasElement.height = this.imagenHTML.height;
+        this.ctx.font = "36pt Verdana";
+
+            this.ctx.clearRect(0,0,this.canvasElement.width,this.canvasElement.height);
+            this.ctx.drawImage(this.imagenHTML, 0, 0);
+            this.ctx.fillStyle = "blue";
+            this.ctx.fillText('kevin',200,120);
+            this.ctx.fillText('34159181',200,220);
+      console.log(this.canvasElement.toDataURL());
+      this.urlImagenCanvas = this.canvasElement.toDataURL();
+    }
 
   formularioWeb(){
     this.navCtrl.setRoot(FormularioWebPage);
