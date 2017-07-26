888df74f62a32a777c66cb1a010a5b8fb22e83e6
Kevin <MasterK@MacBookPro.local>
2017-07-26 15:42:10 -0300

    pdf finish

--- backend/controllers/operacionController.js
+++ backend/controllers/operacionController.js
@@ -168,9 +168,6 @@ var email = function (destino,operacion,oIdOperacion,oFechaTransaccion,oFechaPag
     // fs lee archivos
     var fs = require('fs');
     // embedd js para escribiri variables en html
-    var ejs = require('ejs')
-        , path = __dirname + '/../views/index.ejs'
-        , str = fs.readFileSync(path, 'utf8');
 
     data = {
         'fechaImpresion': oFechaTransaccion,
@@ -187,9 +184,13 @@ var email = function (destino,operacion,oIdOperacion,oFechaTransaccion,oFechaPag
         'tarjeta': oTarjeta,
         'honorariosProfesional': oImporteVenta,
         'montoAcreditado': oImporteCobrar,
+        'cuotas': oCuotas,
+        'importeCuota': oImporteCuota,
+        'codigoAuto': oCodigoAuto,
+        'cupon': oCupon,
+        'mailCliente': oMailCliente,
     }
-    var html = ejs.render(str,data);
-    
+
     config = {
         "format": "A4",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid 
         "orientation": "portrait", // portrait or landscape  
@@ -197,6 +198,22 @@ var email = function (destino,operacion,oIdOperacion,oFechaTransaccion,oFechaPag
         "base": 'file://' + __dirname + '/../img/'
     }   
 
+    switch(destino) {
+        case 'profesional':
+            var ejs = require('ejs')
+            , path = __dirname + '/../views/tamplateProfesional.ejs'
+            , str = fs.readFileSync(path, 'utf8');
+            var html = ejs.render(str,data);
+            break;
+        case 'cliente':
+            var ejs = require('ejs')
+            , path = __dirname + '/../views/tamplateCliente.ejs'
+            , str = fs.readFileSync(path, 'utf8');
+            var html = ejs.render(str,data);
+            break;
+        default:
+    } 
+    
   	var transporter = nodemailer.createTransport({
         host: 'xw000111.ferozo.com',
         port: 587,
