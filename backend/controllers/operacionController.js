5e3363b790a16296ad237ad9f0b79a08d94b3797
Ricky Bruno <ricardobruno_89@hotmail.com>
2017-08-02 22:24:57 -0300

    solucion mail

--- backend/controllers/operacionController.js
+++ backend/controllers/operacionController.js
@@ -64,46 +64,41 @@ exports.operacionNueva = function(req, res, next){
         oDniCliente,oApellidoCliente,oNombreCliente,oTelefonoCliente,oMailCliente,oTarjeta,oImporteVenta,
         oImporteCobrar,oCuotas,oImporteCarga,oImporteCuota,oCodigoAuto,oCupon,function(consulta){
             console.log(consulta);
-            if(!consulta.errno){
                 if(consulta[0].codigo >= 1){
-                var respuesta = consulta[0];
-                var oFechaTransaccion = respuesta.fechaTransaccion;
-                var oFechaPago = respuesta.fechaPago;
-                var oIdOperacion = respuesta.codigo;
-                email('profesional',req.body,oIdOperacion,oFechaTransaccion,oFechaPago,function(res1){
-                    console.log("enviando mail desde operacion nueva: ", res1);
-                    console.log("mail cliente = ",req.body.mailCliente);
-                     if(req.body.mailCliente != ''){
-                         console.log("mail cliente no es vacio, mandando mail");
-                        email('cliente',req.body,oIdOperacion,oFechaTransaccion,oFechaPago,function(res2){
-                            console.log("enviando mail desde operacion nueva: ", res2);
+                    var respuesta = consulta[0];
+                    var oFechaTransaccion = respuesta.fechaTransaccion;
+                    var oFechaPago = respuesta.fechaPago;
+                    var oIdOperacion = respuesta.codigo;
+                    email('profesional',req.body,oIdOperacion,oFechaTransaccion,oFechaPago,function(res1){
+                        console.log("enviando mail desde operacion nueva: ", res1);
+                        console.log("mail cliente = ",req.body.mailCliente);
+                        if(req.body.mailCliente != ''){
+                            console.log("mail cliente no es vacio, mandando mail");
+                            email('cliente',req.body,oIdOperacion,oFechaTransaccion,oFechaPago,function(res2){
+                                console.log("enviando mail desde operacion nueva: ", res2);
+                                let response = {
+                                    'mysql' : consulta,
+                                    'mailProfesional' : res1,
+                                    'mailCliente' : res2
+                                };
+                                console.log(response);
+                                res.json(response);
+                            }); 
+                        }else{
+                            console.log("mail cliente vacio.. no se manda mail.. respondiendo solo mysql y mail prof");
                             let response = {
                                 'mysql' : consulta,
                                 'mailProfesional' : res1,
-                                'mailCliente' : res2
+                                'mailCliente' : 'error'
                             };
-                            console.log(response);
                             res.json(response);
-                        }); 
-                    }else{
-                        console.log("mail cliente vacio.. no se manda mail.. respondiendo solo mysql y mail prof");
-                        let response = {
-                            'mysql' : consulta,
-                            'mailProfesional' : res1,
-                            'mailCliente' : 'error'
-                        };
-                        res.json(response);
-                    }
-                    
-                });
-            }else{
-                consulta = {'mysql' : consulta };
-                res.json(consulta);
-            }
-        }else{
-                consulta = ([{"codigo": 0}]);
-                res.json(consulta);
-            }
+                        }
+                        
+                    });
+                }else{
+                    consulta = {'mysql' : consulta };
+                    res.json(consulta);
+                }
         });
 }
 
