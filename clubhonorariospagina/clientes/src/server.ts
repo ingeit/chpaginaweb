e6a8987c149f168be7eebf5925c591611c47c00a
Ricky Bruno <ricardobruno_89@hotmail.com>
2017-09-28 17:12:52 -0300

    fin clientes

--- clubhonorariospagina/clientes/src/server.ts
+++ clubhonorariospagina/clientes/src/server.ts
@@ -1,6 +1,6 @@
 export let data = {
     // "urlServidor": 'http://rickybruno.sytes.net:3000',
-    // "urlServidor": 'http://104.156.225.203:3000',
-    "urlServidor": 'http://192.168.1.15:3000',
+    "urlServidor": 'http://104.156.225.203:3000',
+    // "urlServidor": 'http://192.168.1.15:3000',
     "password": "false"
 };
