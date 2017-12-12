fb9f8fe00fa7950d1350ebf3a24e7a87e2229ec0
Ricky Bruno <ricardobruno_89@hotmail.com>
2017-12-12 17:20:54 -0300

    limpiando git

--- testMP/backend/config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  host : "localhost",
-  port : "3306",
-  user : "root",
-  password : "soporteit",
-  //password : "",
-  database : "ClubHonorarios"
-};
-
--- testMP/backend/controllers/mysqlController.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('./../models/mysql');
-
-exports.dump = function(req, res, next){
-    mysql.dump(function(consulta){
-        console.log("desde controller mysql: ",consulta);
-        res.json(consulta);
-    });
-}
