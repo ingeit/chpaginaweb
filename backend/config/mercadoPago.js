f22318cbf955455874cfb9907faad0161ce3bb19
Gomez Kevin <masterk@MasterKiMac.local>
2017-12-16 13:36:09 -0300

    pasando a producion mp node

--- backend/config/mercadoPago.js
+++ backend/config/mercadoPago.js
@@ -1,5 +1,5 @@
 module.exports = {
-    access_token : "TEST-4878048892163147-113014-2ae0d006aacfca052ae6e76f1b3bb1d0__LC_LD__-261861182",
+    access_token : "APP_USR-4878048892163147-113014-8d8619df48eb8d66a5863283498fa04e__LA_LC__-261861182",
   };
   
   
